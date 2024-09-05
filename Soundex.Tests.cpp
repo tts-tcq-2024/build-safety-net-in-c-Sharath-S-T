0851716ac8a2a3854c487f16625e6ff04e19983e
Sharath-S-T <sharath.st123@gmail.com>
2024-09-05 16:12:26 +0530

    Updated test cases

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,9 +1,19 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
-TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
- //AAA
-  char soundex[5];
-  generateSoundex("AX", soundex);
- // ASSERT_EQ(soundex,"A200");
+TEST(SoundexTestsuite, ReplacesConsonantsWithAppropriateDigits) 
+{
+    char soundex[5];
+
+    generateSoundex("Sharath", soundex);
+    ASSERT_STREQ(soundex, "S635");
+
+    generateSoundex("aabbccdd", soundex);
+    ASSERT_STREQ(soundex, "A130");
+
+    generateSoundex("", soundex);
+    ASSERT_STREQ(soundex, "");
+
+    generateSoundex("HO1ME", soundex);
+    ASSERT_STREQ(soundex, "H050");
 }
